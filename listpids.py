9be36605aa707c5ad3a5f3c597c8c40b82c8ca61
Cara M Key <carakey@lsu.edu>
2018-12-13 15:43:00 -0600

    variables

--- listpids.py
+++ listpids.py
@@ -1,11 +1,11 @@
-collection = 'hnoc-p16313coll17'
-pidStart = 8539
-pidEnd = 10734
+collection = 'state-lhp'
+pidStart = 10786
+pidEnd = 10856
 pids = range(pidStart, pidEnd)
 
 outFile = open(collection + '_pids.txt' , 'w')
 
 outFile.write(collection + ':')
-outFile.write((',' + collection + ':').join(str(pid) for pid in pids))
+outFile.write(('\n' + collection + ':').join(str(pid) for pid in pids))
 
 outFile.close()
